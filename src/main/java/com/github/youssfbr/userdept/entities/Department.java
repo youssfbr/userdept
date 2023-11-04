d740e39e4659949003474dc75d1babb31d2657c8
Alisson Youssf <youssfbr@gmail.com>
2023-11-04 14:36:31 -0300

    Domain Model

---
+++ src/main/java/com/github/youssfbr/userdept/entities/Department.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.userdept.entities;
+
+public class Department {
+    private Long id;
+    private String name;
+
+    public Department() { }
+    public Department(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+}
