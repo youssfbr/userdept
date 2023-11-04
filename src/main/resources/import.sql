d330271b80e2412c85f7bc12dcfd807267e286f3
Alisson Youssf <youssfbr@gmail.com>
2023-11-04 15:13:27 -0300

    Seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_department (name) VALUES ('Gestão');
+INSERT INTO tb_department (name) VALUES ('Informática');
+
+INSERT INTO tb_user (name, email, department_id) VALUES ('Maria', 'maria@email.com', 1);
+INSERT INTO tb_user (name, email, department_id) VALUES ('Bob', 'bob@email.com', 1);
+INSERT INTO tb_user (name, email, department_id) VALUES ('Alex', 'alex@email.com', 2);
+INSERT INTO tb_user (name, email, department_id) VALUES ('Ana', 'ana@email.com', 2);
