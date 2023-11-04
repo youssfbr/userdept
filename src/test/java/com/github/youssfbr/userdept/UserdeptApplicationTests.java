bb9c4fe9980a8dfdf0d5ae542a00dbaa8356d901
Alisson Youssf <youssfbr@gmail.com>
2023-11-04 14:18:37 -0300

    Project created

---
+++ src/test/java/com/github/youssfbr/userdept/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
