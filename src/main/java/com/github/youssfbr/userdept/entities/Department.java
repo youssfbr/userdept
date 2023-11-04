ddece6617b34d68ce4b586fe6e1b26ced83e6f46
Alisson Youssf <youssfbr@gmail.com>
2023-11-04 14:51:50 -0300

    ORM

--- src/main/java/com/github/youssfbr/userdept/entities/Department.java
+++ src/main/java/com/github/youssfbr/userdept/entities/Department.java
@@ -1,6 +1,14 @@
 package com.github.youssfbr.userdept.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_department")
 public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
--- src/main/java/com/github/youssfbr/userdept/entities/User.java
+++ src/main/java/com/github/youssfbr/userdept/entities/User.java
@@ -1,9 +1,16 @@
 package com.github.youssfbr.userdept.entities;
 
+import jakarta.persistence.*;
+
+@Entity(name = "tb_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
+    @ManyToOne
+    @JoinColumn(name = "department_id")
     private Department department;
 
     public User() { }
